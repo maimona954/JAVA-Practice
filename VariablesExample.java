052b55e5597b23196434c65cac3d2a2f36014346
maimona954 <maimona.farjana@gmail.com>
2026-04-25 20:59:30 +0600

    Java basics (variables and data types)

---
+++ VariablesExample.java
@@ -0,0 +1,15 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 50000.50;
+        char grade = 'A';
+        boolean isTester = true;
+        String name = "Maimona Rahman";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Tester: " + isTester);
+    }
+}
